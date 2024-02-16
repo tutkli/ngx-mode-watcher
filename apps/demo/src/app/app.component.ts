a6dd7a4f2c5557265af7124b3eefd3502c3d9424
Clara Castillo <claracastillochale@gmail.com>
2024-02-16 14:23:07 +0100

    feat: add demo

--- apps/demo/src/app/app.component.ts
+++ apps/demo/src/app/app.component.ts
@@ -1,14 +1,64 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { isPlatformBrowser } from '@angular/common';
+import { Component, computed, inject, PLATFORM_ID } from '@angular/core';
+import { ModeWatcherService } from 'ngx-mode-watcher';
 
 @Component({
-  standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
   selector: 'ngx-mode-watcher-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+  standalone: true,
+  template: `
+    <div class="container py-12 space-y-4 p-4">
+      <p>User prefers mode: {{ modeWatcher.userPrefersMode() }}</p>
+      <p>System prefers mode: {{ modeWatcher.systemPrefersMode() }}</p>
+      <p>Current mode: {{ modeWatcher.mode() }}</p>
+
+      @if (htmlElement() !== undefined) {
+      <pre>{{ htmlElement() }}</pre>
+      } @if (themeColorElement() !== undefined) {
+      <pre>{{ themeColorElement() }}</pre>
+      }
+
+      <div class="flex space-x-2">
+        <button (click)="modeWatcher.toggleMode()">Toggle</button>
+        <button (click)="modeWatcher.setMode('light')">Light Mode</button>
+        <button (click)="modeWatcher.setMode('dark')">Dark Mode</button>
+        <button (click)="modeWatcher.resetMode()">Reset</button>
+      </div>
+    </div>
+  `,
+  styles: `
+    button {
+      @apply bg-blue-500 transition-colors text-white px-2 py-1 rounded-md duration-500;
+    }
+  `,
 })
 export class AppComponent {
-  title = 'demo';
+  private readonly platformId = inject(PLATFORM_ID);
+  protected readonly modeWatcher = inject(ModeWatcherService);
+
+  htmlElement = computed(() => {
+    this.modeWatcher.mode();
+    if (isPlatformBrowser(this.platformId)) {
+      const htmlElement = document.documentElement;
+      if (htmlElement) {
+        return htmlElement.outerHTML.replace(
+          htmlElement.innerHTML + '</html>',
+          ''
+        );
+      }
+    }
+    return undefined;
+  });
+
+  themeColorElement = computed(() => {
+    this.modeWatcher.mode();
+    if (isPlatformBrowser(this.platformId)) {
+      const themeColorElement = document.querySelector(
+        'meta[name="theme-color"]'
+      );
+      if (themeColorElement) {
+        return themeColorElement.outerHTML;
+      }
+    }
+    return undefined;
+  });
 }
--- apps/demo/src/app/app.config.ts
+++ apps/demo/src/app/app.config.ts
@@ -1,7 +1,11 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideModeWatcher } from 'ngx-mode-watcher';
 import { appRoutes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(appRoutes)],
+  providers: [
+    provideRouter(appRoutes),
+    provideModeWatcher({ themeColors: { dark: 'black', light: 'white' } }),
+  ],
 };
